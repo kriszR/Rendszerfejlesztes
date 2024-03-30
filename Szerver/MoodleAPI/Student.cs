09a7d3fbd6ed912b11ef1e8bdbf5bd70d4a35f4e
VeghAtti <77452379+VeghAtti@users.noreply.github.com>
2024-03-30 19:56:33 +0100

    Add files via upload

---
+++ Szerver/MoodleAPI/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodleAPI
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int StudentId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NeptunCode { get; set; }
+    }
+}
