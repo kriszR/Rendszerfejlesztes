be772bda31a6e9fe9f588eefba066b5ad006d64f
VeghAtti <77452379+VeghAtti@users.noreply.github.com>
2024-03-31 00:13:04 +0100

    Add files via upload

---
+++ Szerver/Szerver/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Szerver.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string Course { get; set; } = string.Empty;
+        public string Email {  get; set; } = string.Empty;
+        public string NeptunCode { get; set; } = string.Empty;
+    }
+}
---
+++ Szerver/Szerver/Repositories/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Szerver.Models;
+
+namespace Szerver.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly StudentContext _context;
+
+        public StudentRepository(StudentContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Student> Create(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return student;
+        }
+
+        public async Task Delete(int id)
+        {
+            var studentToDelete = await _context.Students.FindAsync(id);
+            _context.Students.Remove(studentToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Student>> Get()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        public async Task<Student> Get(int id)
+        {
+            return await _context.Students.FindAsync(id);
+        }
+
+        public async Task Update(Student student)
+        {
+            _context.Entry(student).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
