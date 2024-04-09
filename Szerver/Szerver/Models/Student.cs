27b8e1cf74695b1072da009272ce8ecbc5a9a0cd
Mathe1005 <mtvalcz@gmail.com>
2024-04-09 21:29:44 +0200

    Student osztály frissítés

--- Szerver/Szerver/Models/Student.cs
+++ Szerver/Szerver/Models/Student.cs
@@ -3,10 +3,10 @@
     public class Student
     {
         public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string NeptunCode { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public int [] Degrees { get; set; } = [];
+        public string [] MyCourses { get; set; } = [];
     }
 }
