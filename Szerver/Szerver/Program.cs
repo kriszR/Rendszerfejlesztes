f9b5443545f0faf43d227eb859133150df0b8600
Mathe1005 <mtvalcz@gmail.com>
2024-04-15 14:14:01 +0200

    Adatbázis bővítése

--- Szerver/Szerver/Program.cs
+++ Szerver/Szerver/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
-builder.Services.AddDbContext<StudentContext>(o => o.UseSqlite("Data source=students.db"));
+builder.Services.AddDbContext<MoodleDbContext>(o => o.UseSqlite("Data source=Moodle.db"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
