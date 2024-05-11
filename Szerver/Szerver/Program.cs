7a4fff8d33191c3af7d089f5c954be990eba8a89
Mathe1005 <mtvalcz@gmail.com>
2024-05-11 16:01:34 +0200

    Szerver oldali websocket

---
+++ Szerver/Szerver/Program.cs
@@ -0,0 +1,136 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using Szerver.Models;
+using Szerver.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Builder;
+using Szerver.WebSocket;
+using Szerver.WebSocket.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+//szeva
+// Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IApprovedDegreesRepository, ApprovedDegreesRepository>();
+builder.Services.AddScoped<IMyCoursesRepository, MyCoursesRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IDegreeRepository, DegreeRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddSingleton<MoodleHandler>();
+builder.Services.AddSqlite<MoodleContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddControllers();
+builder.Services.AddWebSocketManager();
+
+// Authentication configuration
+/*var key = Encoding.ASCII.GetBytes("A_titkos_kulcs_szoveg");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});*/
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true
+                    };
+                });
+
+
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Szerver", Version = "v1" });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, Array.Empty<string>() }
+    });
+});
+
+// CORS policy configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(
+        c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Szerver v1")
+        );
+}
+
+
+
+// Apply HTTPS redirection (before CORS)
+app.UseHttpsRedirection();
+
+// Apply CORS policy (before authentication and authorization)
+app.UseCors("AllowOrigin");
+
+//WebSocket
+var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+var serviceProvider = serviceScopeFactory.CreateScope().ServiceProvider;
+app.UseWebSockets();
+app.MapWebSocketManager("/Szerver/ws", app.Services.GetRequiredService<MoodleHandler>());
+
+
+// Authentication and Authorization middleware
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Map controllers
+app.MapControllers();
+
+// Default route
+app.Run();
---
+++ Szerver/Szerver/WebSocket/Handlers/MoodleHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace Szerver.WebSocket.Handlers
+{
+    public class MoodleHandler : WebSocketHandler
+    {
+        public MoodleHandler(ConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+        }
+
+        public override async Task ReceiveAsync(System.Net.WebSockets.WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var message = $"Új esemény lett rögzítve: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+
+            await SendMessageToAllAsync(message);
+        }
+    }
+}
